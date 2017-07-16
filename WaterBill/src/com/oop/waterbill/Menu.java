2809e990aec00f22a6521b5f44ce3f4bc6b8417a
Marjay Tapay <tapaymarjay@gmail.com>
2017-07-16 11:30:26 +0800

    MenuTabs

--- WaterBill/src/com/oop/waterbill/Menu.java
+++ WaterBill/src/com/oop/waterbill/Menu.java
@@ -5,12 +5,16 @@
  */
 package com.oop.waterbill;
 
+import java.awt.Color;
+import javax.swing.ImageIcon;
+
 /**
  *
  * @author User
  */
 public class Menu extends javax.swing.JFrame {
-
+    final Color blue = new Color(33   , 63, 87);
+    final Color white = new Color(253  , 253, 253);
     /**
      * Creates new form Menu
      */
@@ -29,16 +33,16 @@ public class Menu extends javax.swing.JFrame {
 
         jPanel1 = new javax.swing.JPanel();
         jPanel2 = new javax.swing.JPanel();
-        jPanel4 = new javax.swing.JPanel();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel1 = new javax.swing.JLabel();
+        paymentpanel = new javax.swing.JPanel();
+        paymenticon = new javax.swing.JLabel();
+        paymenttext = new javax.swing.JLabel();
         jPanel5 = new javax.swing.JPanel();
-        jPanel6 = new javax.swing.JPanel();
-        jLabel4 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
-        jPanel7 = new javax.swing.JPanel();
-        jLabel5 = new javax.swing.JLabel();
-        jLabel6 = new javax.swing.JLabel();
+        recordpanel = new javax.swing.JPanel();
+        recordtext = new javax.swing.JLabel();
+        recordicon = new javax.swing.JLabel();
+        inputpanel = new javax.swing.JPanel();
+        inputtext = new javax.swing.JLabel();
+        inputicon = new javax.swing.JLabel();
         jPanel3 = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -48,56 +52,71 @@ public class Menu extends javax.swing.JFrame {
         jPanel2.setBackground(new java.awt.Color(33, 63, 87));
         jPanel2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jPanel4.setBackground(new java.awt.Color(33, 63, 87));
-        jPanel4.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+        paymentpanel.setBackground(new java.awt.Color(33, 63, 87));
+        paymentpanel.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                paymentpanelMousePressed(evt);
+            }
+        });
+        paymentpanel.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/oop/waterbill/Paycheque_35px.png"))); // NOI18N
-        jPanel4.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, -1, 20));
+        paymenticon.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        paymenticon.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/oop/waterbill/Paycheque_35px.png"))); // NOI18N
+        paymentpanel.add(paymenticon, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, -1, 20));
 
-        jLabel1.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
-        jLabel1.setForeground(new java.awt.Color(253, 253, 253));
-        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel1.setText("Payments");
-        jPanel4.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 10, 90, 20));
+        paymenttext.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
+        paymenttext.setForeground(new java.awt.Color(253, 253, 253));
+        paymenttext.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        paymenttext.setText("Payments");
+        paymentpanel.add(paymenttext, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 10, 90, 20));
 
-        jPanel2.add(jPanel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 90, 190, 40));
+        jPanel2.add(paymentpanel, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 90, 190, 40));
 
         jPanel5.setMinimumSize(new java.awt.Dimension(170, 47));
         jPanel5.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
         jPanel2.add(jPanel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 160, -1, 50));
 
-        jPanel6.setBackground(new java.awt.Color(33, 63, 87));
-        jPanel6.setPreferredSize(new java.awt.Dimension(170, 30));
-        jPanel6.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+        recordpanel.setBackground(new java.awt.Color(33, 63, 87));
+        recordpanel.setPreferredSize(new java.awt.Dimension(170, 30));
+        recordpanel.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                recordpanelMousePressed(evt);
+            }
+        });
+        recordpanel.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel4.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
-        jLabel4.setForeground(new java.awt.Color(253, 253, 253));
-        jLabel4.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel4.setText("Records");
-        jPanel6.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 10, -1, 20));
+        recordtext.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
+        recordtext.setForeground(new java.awt.Color(253, 253, 253));
+        recordtext.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        recordtext.setText("Records");
+        recordpanel.add(recordtext, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 10, -1, 20));
 
-        jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/oop/waterbill/Business Contact_35px_1.png"))); // NOI18N
-        jLabel3.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
-        jPanel6.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, 30, 20));
+        recordicon.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        recordicon.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/oop/waterbill/Business Contact_35px_1.png"))); // NOI18N
+        recordicon.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        recordpanel.add(recordicon, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, 30, 20));
 
-        jPanel2.add(jPanel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 160, 190, 40));
+        jPanel2.add(recordpanel, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 160, 190, 40));
 
-        jPanel7.setBackground(new java.awt.Color(33, 63, 87));
-        jPanel7.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+        inputpanel.setBackground(new java.awt.Color(33, 63, 87));
+        inputpanel.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                inputpanelMousePressed(evt);
+            }
+        });
+        inputpanel.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel5.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
-        jLabel5.setForeground(new java.awt.Color(253, 253, 253));
-        jLabel5.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel5.setText("Input Reading");
-        jPanel7.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 10, -1, 20));
+        inputtext.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
+        inputtext.setForeground(new java.awt.Color(253, 253, 253));
+        inputtext.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        inputtext.setText("Input Reading");
+        inputpanel.add(inputtext, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 10, -1, 20));
 
-        jLabel6.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/oop/waterbill/Plus_25px.png"))); // NOI18N
-        jPanel7.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 5, 30, -1));
+        inputicon.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        inputicon.setIcon(new javax.swing.ImageIcon(getClass().getResource("/com/oop/waterbill/Plus_25px.png"))); // NOI18N
+        inputpanel.add(inputicon, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 5, 30, -1));
 
-        jPanel2.add(jPanel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 230, 190, 40));
+        jPanel2.add(inputpanel, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 230, 190, 40));
 
         jPanel1.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 210, 520));
 
@@ -129,6 +148,32 @@ public class Menu extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void paymentpanelMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_paymentpanelMousePressed
+        // TODO add your handling code here:
+        
+        paymenttext.setForeground(blue);
+        paymenticon.setIcon(new ImageIcon("src\\com\\oop\\waterbill\\Paycheque_35px_2.png"));
+        paymentpanel.setBackground(white);
+        resetColor(1);
+        
+    }//GEN-LAST:event_paymentpanelMousePressed
+
+    private void recordpanelMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_recordpanelMousePressed
+        // TODO add your handling code here:
+        recordtext.setForeground(blue);
+        recordicon.setIcon(new ImageIcon("src\\com\\oop\\waterbill\\Business Contact_35px_2.png"));
+        recordpanel.setBackground(white);
+        resetColor(2);
+    }//GEN-LAST:event_recordpanelMousePressed
+
+    private void inputpanelMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_inputpanelMousePressed
+        // TODO add your handling code here:
+        inputtext.setForeground(blue);
+        inputicon.setIcon(new ImageIcon("src\\com\\oop\\waterbill\\Plus_25px_1.png"));
+        inputpanel.setBackground(white);
+        resetColor(3);
+    }//GEN-LAST:event_inputpanelMousePressed
 
     /**
      * @param args the command line arguments
@@ -165,19 +210,58 @@ public class Menu extends javax.swing.JFrame {
         });
     }
 
+    public void resetColor(int choice){
+    
+        switch(choice){
+            case 1:
+                recordtext.setForeground(white);
+        recordicon.setIcon(new ImageIcon("src\\com\\oop\\waterbill\\Business Contact_35px_1.png"));
+        recordpanel.setBackground(blue);
+        inputtext.setForeground(white);
+        inputicon.setIcon(new ImageIcon("src\\com\\oop\\waterbill\\Plus_25px.png"));
+        inputpanel.setBackground(blue);
+                break;
+            case 2:
+                 inputtext.setForeground(white);
+        inputicon.setIcon(new ImageIcon("src\\com\\oop\\waterbill\\Plus_25px.png"));
+        inputpanel.setBackground(blue);
+         paymenttext.setForeground(white);
+        paymenticon.setIcon(new ImageIcon("src\\com\\oop\\waterbill\\Paycheque_35px.png"));
+        paymentpanel.setBackground(blue);
+                break;
+            case 3:
+                paymenttext.setForeground(white);
+        paymenticon.setIcon(new ImageIcon("src\\com\\oop\\waterbill\\Paycheque_35px.png"));
+        paymentpanel.setBackground(blue);
+        recordtext.setForeground(white);
+        recordicon.setIcon(new ImageIcon("src\\com\\oop\\waterbill\\Business Contact_35px_1.png"));
+        recordpanel.setBackground(blue);
+                break;
+            default:
+                break;
+        
+        
+        
+        }
+    
+    
+    }
+    
+    
+    
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel inputicon;
+    private javax.swing.JPanel inputpanel;
+    private javax.swing.JLabel inputtext;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
-    private javax.swing.JPanel jPanel4;
     private javax.swing.JPanel jPanel5;
-    private javax.swing.JPanel jPanel6;
-    private javax.swing.JPanel jPanel7;
+    private javax.swing.JLabel paymenticon;
+    private javax.swing.JPanel paymentpanel;
+    private javax.swing.JLabel paymenttext;
+    private javax.swing.JLabel recordicon;
+    private javax.swing.JPanel recordpanel;
+    private javax.swing.JLabel recordtext;
     // End of variables declaration//GEN-END:variables
 }
