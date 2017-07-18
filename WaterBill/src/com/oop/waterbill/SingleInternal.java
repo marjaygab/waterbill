aef4c9fe0764457be35a3eb79527d9b827afc241
Kenneth Ocampo <kennethocampo2809@gmail.com>
2017-07-18 00:37:56 -0700

    InputPanel Partially finished

--- WaterBill/src/com/oop/waterbill/SingleInternal.java
+++ WaterBill/src/com/oop/waterbill/SingleInternal.java
@@ -59,6 +59,7 @@ public class SingleInternal extends javax.swing.JPanel {
         clearbuttoninternal = new javax.swing.JButton();
         finishbuttoninternal = new javax.swing.JButton();
         confirmbuttoninternal = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
 
         jPanel1.setBackground(new java.awt.Color(253, 253, 253));
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -67,10 +68,12 @@ public class SingleInternal extends javax.swing.JPanel {
         jLabel6.setText("Last Name");
         jPanel1.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 90, -1, -1));
 
+        lastsingleinternal.setEditable(false);
         lastsingleinternal.setBackground(new java.awt.Color(253, 253, 253));
         lastsingleinternal.setForeground(new java.awt.Color(51, 51, 51));
         jPanel1.add(lastsingleinternal, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 110, 140, -1));
 
+        firstsingleinternal.setEditable(false);
         firstsingleinternal.setBackground(new java.awt.Color(253, 253, 253));
         firstsingleinternal.setForeground(new java.awt.Color(51, 51, 51));
         jPanel1.add(firstsingleinternal, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 110, 150, -1));
@@ -79,6 +82,7 @@ public class SingleInternal extends javax.swing.JPanel {
         jLabel7.setText("First Name");
         jPanel1.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 90, -1, -1));
 
+        middlesingleinternal.setEditable(false);
         middlesingleinternal.setBackground(new java.awt.Color(253, 253, 253));
         middlesingleinternal.setForeground(new java.awt.Color(51, 51, 51));
         jPanel1.add(middlesingleinternal, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 110, 120, -1));
@@ -87,6 +91,7 @@ public class SingleInternal extends javax.swing.JPanel {
         jLabel8.setText("Middle Name");
         jPanel1.add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 90, -1, -1));
 
+        agesingleinternal.setEditable(false);
         agesingleinternal.setBackground(new java.awt.Color(253, 253, 253));
         agesingleinternal.setForeground(new java.awt.Color(51, 51, 51));
         jPanel1.add(agesingleinternal, new org.netbeans.lib.awtextra.AbsoluteConstraints(500, 110, 50, -1));
@@ -108,6 +113,7 @@ public class SingleInternal extends javax.swing.JPanel {
         });
         jPanel1.add(accountsingeinternal, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 60, 110, -1));
 
+        gendersingleinternal.setEditable(false);
         gendersingleinternal.setBackground(new java.awt.Color(253, 253, 253));
         gendersingleinternal.setForeground(new java.awt.Color(51, 51, 51));
         jPanel1.add(gendersingleinternal, new org.netbeans.lib.awtextra.AbsoluteConstraints(580, 110, 50, -1));
@@ -116,6 +122,7 @@ public class SingleInternal extends javax.swing.JPanel {
         jLabel11.setText("Area");
         jPanel1.add(jLabel11, new org.netbeans.lib.awtextra.AbsoluteConstraints(480, 150, 70, -1));
 
+        homesingleinternal.setEditable(false);
         homesingleinternal.setBackground(new java.awt.Color(253, 253, 253));
         homesingleinternal.setForeground(new java.awt.Color(51, 51, 51));
         jPanel1.add(homesingleinternal, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 170, 370, -1));
@@ -134,6 +141,7 @@ public class SingleInternal extends javax.swing.JPanel {
         jPanel1.add(jLabel14, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 220, 70, -1));
         jPanel1.add(coordinternalcombo, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 240, 110, -1));
 
+        metersingleinternal.setEditable(false);
         metersingleinternal.setBackground(new java.awt.Color(253, 253, 253));
         metersingleinternal.setForeground(new java.awt.Color(51, 51, 51));
         jPanel1.add(metersingleinternal, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 240, 120, -1));
@@ -189,6 +197,11 @@ public class SingleInternal extends javax.swing.JPanel {
         clearbuttoninternal.setText("Clear");
         clearbuttoninternal.setContentAreaFilled(false);
         clearbuttoninternal.setOpaque(true);
+        clearbuttoninternal.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                clearbuttoninternalActionPerformed(evt);
+            }
+        });
 
         finishbuttoninternal.setBackground(new java.awt.Color(134, 207, 190));
         finishbuttoninternal.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
@@ -203,6 +216,11 @@ public class SingleInternal extends javax.swing.JPanel {
         confirmbuttoninternal.setText("Cofirm");
         confirmbuttoninternal.setContentAreaFilled(false);
         confirmbuttoninternal.setOpaque(true);
+        confirmbuttoninternal.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                confirmbuttoninternalActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
         jPanel3.setLayout(jPanel3Layout);
@@ -230,6 +248,14 @@ public class SingleInternal extends javax.swing.JPanel {
 
         jPanel1.add(jPanel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 280, 330, 80));
 
+        jButton1.setText("Ok");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 60, 50, 20));
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
@@ -250,6 +276,51 @@ public class SingleInternal extends javax.swing.JPanel {
         // TODO add your handling code here:
     }//GEN-LAST:event_accountsingeinternalActionPerformed
 
+    private void confirmbuttoninternalActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_confirmbuttoninternalActionPerformed
+        // TODO add your handling code here:
+        
+        
+        
+        
+        
+        
+        
+    }//GEN-LAST:event_confirmbuttoninternalActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+       A1Single_Bulk_Input  ok =  new A1Single_Bulk_Input();
+       ok.getAccNum(accountsingeinternal.getText());
+       
+       lastsingleinternal.setText(ok.getlast_());
+       firstsingleinternal.setText(ok.getfirst_());
+       middlesingleinternal.setText(ok.getmiddle_());
+       agesingleinternal.setText(ok.getage_());
+       gendersingleinternal.setText(ok.getgender_());
+       homesingleinternal.setText(ok.gethomeadd());
+       metersingleinternal.setText(ok.getmeterno());
+               
+
+       
+       
+        
+        
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void clearbuttoninternalActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_clearbuttoninternalActionPerformed
+        // TODO add your handling code here:
+        lastsingleinternal.setText("");
+        firstsingleinternal.setText("");
+        middlesingleinternal.setText("");
+        agesingleinternal.setText("");
+        gendersingleinternal.setText("");
+        homesingleinternal.setText("");
+        metersingleinternal.setText("");
+        accountsingeinternal.setText("");
+        
+        
+    }//GEN-LAST:event_clearbuttoninternalActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTextField accountsingeinternal;
@@ -263,6 +334,7 @@ public class SingleInternal extends javax.swing.JPanel {
     private javax.swing.JTextField firstsingleinternal;
     private javax.swing.JTextField gendersingleinternal;
     private javax.swing.JTextField homesingleinternal;
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel11;
     private javax.swing.JLabel jLabel12;
